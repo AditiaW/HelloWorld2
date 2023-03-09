9377c9d1ad289499e5588d02c9fa35d0ec7b6d1a
Aditia Winanda <aditiaw1507@gmail.com>
2023-03-09 11:01:11 +0700

    test

---
+++ app/src/main/java/com/aditiawinanda202102310/helloworld/MainActivity.java
@@ -0,0 +1,14 @@
+package com.aditiawinanda202102310.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
